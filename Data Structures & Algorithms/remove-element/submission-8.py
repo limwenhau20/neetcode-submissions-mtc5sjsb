7d1a192329edea923f63ec20e09da64d59e0bc2d
limwenhau20 <167644162+limwenhau20@users.noreply.github.com>
2026-07-05 02:24:23 +0200

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        ##### Easy to understand (inplace)
+        # position = 0
+        # for num in nums:
+        #     if num != val:
+        #         nums[position] = num
+        #         position += 1
+        # return position
+
+
+        ##### HARD to understand (inplace)
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                n -= 1
+                nums[i] = nums[n]
+            else:
+                i += 1
+        return n
